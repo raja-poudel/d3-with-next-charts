7ad85c69138181dfd6b6d036f600fd924c233f22
Raja Poudel <raja.poudel@myanmarwebstore.com>
2022-05-19 16:48:56 +0630

    added three line chart

--- src/components/charts/t5-line-chart.js
+++ src/components/charts/t5-line-chart.js
@@ -16,14 +16,77 @@ export const T5LineChart = ({ data, height = 350 }) => {
                 .attr("transform", `translate(${margin.left}, ${margin.top})`);
         // let parseTime = d3.timeParse("%Y-%m-%d"); //only one will work fine
         // var parseTime = d3.timeParse("%d-%b-%y"); 
-        let newData = data.series.map((d, i) => {
-            return {
-                ...d, data: d.data.map((d, j) => {
-                    return { x: data.xaxis.dataPoints[j], y: d }
-                })
-            }
-        })
-        console.log(newData);
+
+        let y0Data = data.series[0],
+            y1Data = data.series[1],
+            y2Data = data.series[2];
+
+        let newy0Data = y0Data.data.map((d, i) => {
+            return { x: data.xaxis.dataPoints[i], y: d }
+        }),
+            newy1Data = y1Data.data.map((d, i) => {
+                return { x: data.xaxis.dataPoints[i], y: d }
+            }),
+            newy2Data = y2Data.data.map((d, i) => {
+                return { x: data.xaxis.dataPoints[i], y: d }
+            });
+        let x = d3.scalePoint().domain(data.xaxis.dataPoints).range([0, width]),
+            y0 = d3.scaleLinear().domain([0, d3.max(y0Data.data, function (d) { return d })]).range([height, 0]),
+            y1 = d3.scaleLinear().domain([0, d3.max(y1Data.data, function (d) { return d })]).range([height, 0]),
+            y2 = d3.scaleLinear().domain([0, d3.max(y2Data.data, function (d) { return d })]).range([height, 0]);
+
+        let xAxis = d3.axisBottom(x),
+            yAxisLeft = d3.axisLeft(y0).ticks(5),
+            y1AxisLeft = d3.axisLeft(y1).ticks(5),
+            yAxisRight = d3.axisRight(y2).ticks(5);
+        main.append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .call(xAxis);
+
+        main.append("g")
+            .call(yAxisLeft);
+        main.append("g")
+            .call(y1AxisLeft);
+        main.append("g")
+            .attr("transform", `translate(${width}, 0)`)
+            .call(yAxisRight);
+
+        let valueLine0 = d3.line()
+            .x(function (d) {
+                return x(d.x)
+            })
+            .y(function (d) { return y0(d.y) })
+            .curve(d3.curveCardinal)
+
+        let valueLine1 = d3.line()
+            .x(function (d) { return x(d.x) })
+            .y(function (d) { return y1(d.y) })
+            .curve(d3.curveCardinal)
+        let valueLine2 = d3.line()
+            .x(function (d) { return x(d.x) })
+            .y(function (d) { return y2(d.y) })
+            .curve(d3.curveCardinal);
+
+
+        main.append("path")
+            .style('stroke', "orange")
+            .style("fill", "none")
+            .style("stroke-width", 2)
+            .attr("d", valueLine0(newy0Data))
+
+
+        main.append("path")
+            .style("stroke", "red")
+            .style("fill", "none")
+            .attr("d", valueLine1(newy1Data))
+            .style('stroke-width', 2)
+
+        // main.append("path")
+        //     .style("stroke", "green")
+        //     .style("fill", "none")
+        //     .attr("d", valueLine2(newy2Data))
+        //     .style('stroke-width', 2)
+
     }, [data.length]);
     return (
         <>
